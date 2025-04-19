197fde5e2434a9b82d32e9ed4972c371251c32cc
Hak Lee <me@haklee.me>
2025-04-19 14:10:51 +0900

    build: add vanilla-extract dependencies

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
+import { createVanillaExtractPlugin } from '@vanilla-extract/next-plugin';
 import type { NextConfig } from 'next';
 import { withContentlayer } from 'next-contentlayer2';
 
+const withVanillaExtract = createVanillaExtractPlugin();
+
 const nextConfig: NextConfig = {};
 
-export default withContentlayer(nextConfig);
+export default withContentlayer(withVanillaExtract(nextConfig));
