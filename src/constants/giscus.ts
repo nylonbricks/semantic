4d6e11517de772fa8cbbcc724a2e935478659e53
Hak Lee <me@haklee.me>
2025-04-24 13:56:04 +0900

    refactor: make `Giscus` config controllable via constants

---
+++ src/constants/giscus.ts
@@ -0,0 +1,7 @@
+export const GISCUS = {
+  REPO: 'nylon-bricks/semantic',
+  REPO_ID: 'R_kgDOMljSQQ',
+  CATEGORY: 'General',
+  CATEGORY_ID: 'DIC_kwDOMljSQc4CpWhQ',
+  MAPPING: 'title',
+} as const;
