932e61c2f726f8b30b77c5a9207b95d95e08d084
Hak Lee <hi@haklee.me>
2026-01-31 12:56:01 +0900

    chore(lint): migrate to ultracite and remove eslint/prettier

--- eslint.config.mjs
+++
@@ -1,8 +0,0 @@
-import { config } from '@hakui/eslint-config/next-js';
-
-/** @type {import('eslint').Linter.Config} */
-export default [
-  ...config,
-  { rules: { 'react/prop-types': 'off' }, },
-  { ignores: ['.next/**'], },
-];
